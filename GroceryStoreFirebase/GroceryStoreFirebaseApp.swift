1070a734033bbbc042f87b1a84292b73b4be3909
nadlev <nadiya.levytska.jr@gmail.com>
2023-04-23 21:52:08 +0300

    Initial Commit

---
+++ GroceryStoreFirebase/GroceryStoreFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFirebaseApp.swift
+//  GroceryStoreFirebase
+//
+//  Created by Надежда Левицкая on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
